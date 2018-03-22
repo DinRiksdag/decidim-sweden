0ef3870ff4d970454727a0c5cbffd3218acd35d5
Pierre Mesure <pierre.mesure@gmail.com>
2018-03-22 16:44:41 +0100

    Changed the app and e-mail sender name

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Decidim.configure do |config|
-  config.application_name = "My Application Name"
-  config.mailer_sender = "change-me@domain.org"
+  config.application_name = "Din Riksdag"
+  config.mailer_sender = "Din Riksdag"
 
   # Change these lines to set your preferred locales
   config.default_locale = :en
